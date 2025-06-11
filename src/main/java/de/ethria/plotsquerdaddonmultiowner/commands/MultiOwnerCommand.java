e5cde6a5cf09bb0a706769ce570490df91c884a2
Michael Bork <michael.bork@rwofan.de>
2025-06-11 11:31:50 +0200

    Updates, Bugfixes, add Permissions, Modify Database

--- src/main/java/de/ethria/plotsquerdaddonmultiowner/commands/MultiOwnerCommand.java
+++ src/main/java/de/ethria/plotsquerdaddonmultiowner/commands/MultiOwnerCommand.java
@@ -1,7 +1,6 @@
 package de.ethria.plotsquerdaddonmultiowner.commands;
 
 import org.bukkit.Bukkit;
-import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
